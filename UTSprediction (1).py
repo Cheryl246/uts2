7a36d6124ada641bb1f3e1e7698d2d7919f3da27
Cheryl246 <cheryl.valencia@binus.ac.id>
2025-04-20 20:50:55 +0700

    Update UTSprediction (1).py

--- UTSprediction (1).py
+++ UTSprediction (1).py
@@ -1,7 +1,8 @@
+import streamlit as st
+import pickle
 import pandas as pd
 import numpy as np
 import joblib
-import streamlit as st
 
 model = joblib.load('Ranfor_train1 (1).pkl')
 target_encoded = joblib.load('target_encoded.pkl')
@@ -10,101 +11,64 @@ room_type = joblib.load('room_type.pkl')
 market_segment = joblib.load('market_segment (2).pkl')
 
 def preprocess_data(data):
-    df = pd.DataFrame([list(data.values())], columns=[
-        'Booking ID', 'Number of adults', 'Number of children', 
-        'Number of weekend nights', 'Number of week nights', 'Type of meal plan', 
-        'Required car parking space', 'Room type reserved', 'Lead time', 
-        'Arrival year', 'Arrival month', 'Arrival date', 'Market segment type', 
-        'Repeated guest', 'Number of previous cancellations', 
-        'Number of previous bookings not canceled', 'Average price per room', 
-        'Number of special requests'
-    ])
-    
+    df = pd.DataFrame([data], columns=['Booking ID', 'Number of adults', 'Number of children', 
+                                       'Number of weekend nights', 'Number of week nights', 'Type of meal plan', 
+                                       'Required car parking space', 'Room type reserved', 'Lead time', 
+                                       'Arrival year', 'Arrival month', 'Arrival date', 'Market segment type', 
+                                       'Repeated guest', 'Number of previous cancellations', 
+                                       'Number of previous bookings not canceled', 'Average price per room', 
+                                       'Number of special requests'])
+
     df = df.replace(meal_plan)
     df = df.replace(room_type)
     df = pd.get_dummies(df, columns=['Market segment type'], drop_first=True)
+    
     df.drop(['Booking ID'], axis=1, inplace=True)
     df.replace('', np.nan, inplace=True)
-    df.fillna(df.median(), inplace=True)  
-    
-    # Ensure all columns that need to be numeric are converted to float
-    df = df.apply(pd.to_numeric, errors='coerce')  # Convert any non-numeric data to NaN and then handle it
-    
-    # Handle any NaN values again (if any NaNs remain after coercion, fill them with median)
+    df.fillna(df.median(), inplace=True)
+
+    df = df.apply(pd.to_numeric, errors='coerce')
+    df.fillna(df.median(), inplace=True)
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
     df.fillna(df.median(), inplace=True)
     
-    # Handle infinite values (if any)
-    df.replace([np.inf, -np.inf], np.nan, inplace=True)  # Replace infinity with NaN
-    df.fillna(df.median(), inplace=True)  # Fill NaN values again with the median
-
     return df
 
 def make_prediction(features):
     input_array = np.array(features).reshape(1, -1)
-
     if np.any(np.isnan(input_array)) or np.any(np.isinf(input_array)):
         raise ValueError("Input contains NaN or infinite values.")
-
-    prediction = model.predict(input_array)
-    return prediction[0]
+    return model.predict(input_array)[0]
 
 def main():
     st.title('Hotel Reservation Prediction')
-    Booking_ID = st.text_input("Booking_ID")
-    no_of_adults = st.selectbox("Adults", options=range(1,11))
-    no_of_children = st.selectbox("Children, under 17", options=range(1,11))
-    no_of_weekend_nights = st.number_input("Weekend Nights", 0, 8)
-    no_of_week_nights = st.number_input("Week Nights (Mon to Fri)", 0, 7)
-    type_of_meal_plan = st.selectbox("Type of meal plan", ["Meal Plan 1", "Meal Plan 2", "Meal Plan 3","Not Selected"])
-    required_car_parking_space = st.radio("Required car parking space", [0, 1])
-    room_type_reserved = st.selectbox("Room type reserved", ["Room_Type 1", "Room_Type 2", "Room_Type 3", "Room_Type 4","Room_Type 5","Room_Type 6","Room_Type 7"])
-    lead_time = st.number_input("Lead time (in days)", 0, 365)
-    arrival_year = st.number_input("Arrival year", 2016, 2020)
-    arrival_month = st.number_input("Arrival month", 0, 13)
-    arrival_date = st.number_input("Arrival date",0,31)
-    market_segment_type = st.radio("Market segment type", ["Online", "Corporate", "Complementary", "Offline","Aviation"])
-    repeated_guest = st.radio("Repeated guest", [0, 1])
-    no_of_previous_cancellations = st.number_input("Number of previous cancellations", 0, 30)
-    no_of_previous_bookings_not_canceled = st.number_input("Number of previous bookings not canceled", 0, 30)
-    avg_price_per_room = st.number_input("Average price per room (in Euros)",0.0, 1000.0)
-    no_of_special_requests = st.number_input("Number of special requests", 0, 10)
 
     data = {
-        'Booking ID': Booking_ID,
-        'Number of adults': no_of_adults,
-        'Number of children': no_of_children,
-        'Number of weekend nights': no_of_weekend_nights,
-        'Number of week nights': no_of_week_nights,
-        'Type of meal plan': type_of_meal_plan,
-        'Required car parking space': required_car_parking_space,
-        'Room type reserved': room_type_reserved,
-        'Lead time': lead_time,
-        'Arrival year': arrival_year,
-        'Arrival month': arrival_month,
-        'Arrival date': arrival_date,
-        'Market segment type': market_segment_type,
-        'Repeated guest': repeated_guest,
-        'Number of previous cancellations': no_of_previous_cancellations,
-        'Number of previous bookings not canceled': no_of_previous_bookings_not_canceled,
-        'Average price per room': avg_price_per_room,
-        'Number of special requests': no_of_special_requests }
+        'Booking ID': st.text_input("Booking_ID"),
+        'Number of adults': st.selectbox("Adults", options=range(1, 11)),
+        'Number of children': st.selectbox("Children, under 17", options=range(1, 11)),
+        'Number of weekend nights': st.number_input("Weekend Nights", 0, 8),
+        'Number of week nights': st.number_input("Week Nights (Mon to Fri)", 0, 7),
+        'Type of meal plan': st.selectbox("Type of meal plan", ["Meal Plan 1", "Meal Plan 2", "Meal Plan 3", "Not Selected"]),
+        'Required car parking space': st.radio("Required car parking space", [0, 1]),
+        'Room type reserved': st.selectbox("Room type reserved", ["Room_Type 1", "Room_Type 2", "Room_Type 3", "Room_Type 4", "Room_Type 5", "Room_Type 6", "Room_Type 7"]),
+        'Lead time': st.number_input("Lead time (in days)", 0, 365),
+        'Arrival year': st.number_input("Arrival year", 2016, 2020),
+        'Arrival month': st.number_input("Arrival month", 1, 12),
+        'Arrival date': st.number_input("Arrival date", 1, 31),
+        'Market segment type': st.radio("Market segment type", ["Online", "Corporate", "Complementary", "Offline", "Aviation"]),
+        'Repeated guest': st.radio("Repeated guest", [0, 1]),
+        'Number of previous cancellations': st.number_input("Number of previous cancellations", 0, 30),
+        'Number of previous bookings not canceled': st.number_input("Number of previous bookings not canceled", 0, 30),
+        'Average price per room': st.number_input("Average price per room (in Euros)", 0.0, 1000.0),
+        'Number of special requests': st.number_input("Number of special requests", 0, 10)
+    }
 
-    df = pd.DataFrame([list(data.values())], columns=['Booking ID', 'Number of adults', 'Number of children', 'Number of weekend nights', 
-                                                  'Number of week nights', 'Type of meal plan', 'Required car parking space', 
-                                                  'Room type reserved', 'Lead time', 'Arrival year', 'Arrival month', 'Arrival date', 
-                                                  'Market segment type', 'Repeated guest', 'Number of previous cancellations', 
-                                                  'Number of previous bookings not canceled', 'Average price per room', 
-                                                  'Number of special requests'])
-
-    # Preprocess the data
     df = preprocess_data(data)
 
-    # Predict the result
     if st.button('Make Prediction'):
         result = make_prediction(df)
         st.success(f'The prediction is: {result}')
 
-# Run the Streamlit app
 if __name__ == '__main__':
     main()
-
